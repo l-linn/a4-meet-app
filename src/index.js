6856c7b631799ead068d84d676367acf1c9ed049
lin <lin7cb@outlook.com>
2024-03-10 15:08:15 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import * as atatus from 'atatus-spa';
-atatus.config('5581856406b848c9bdb73e1f25de7c34').install();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
@@ -24,5 +21,3 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-atatus.notify(new Error('Test Atatus Setup'));
