82a7faa52f328b45a9024d6004d8c07d7edca098
lin <lin7cb@outlook.com>
2024-03-11 19:58:45 +0000

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,7 +6,7 @@ const SCOPES = [
     'https://www.googleapis.com/auth/calendar.events.public.readonly',
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ['https://l-linn.github.io/meet/'];
+const redirect_uris = ['https://l-linn.github.io/a4-meet-app/'];
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
