900fb822eb160734e60ee8b5164f2b1df80d1d1b
lin <lin7cb@outlook.com>
2024-03-12 21:01:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -17,6 +18,8 @@ const App = () => {
     const [currentNOE, setCurrentNOE] = useState(32);
     const [events, setEvents] = useState([]);
     const [currentCity, setCurrentCity] = useState('See all cities');
+    const [infoAlert, setInfoAlert] = useState('');
+    const [errorAlert, setErrorAlert] = useState('');
 
     useEffect(() => {
         fetchData();
@@ -34,11 +37,20 @@ const App = () => {
 
     return (
         <div className='App'>
+            <div className='alerts-container'>
+                {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+                {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+            </div>
             <CitySearch
                 allLocations={allLocations}
                 setCurrentCity={setCurrentCity}
+                setInfoAlert={setInfoAlert}
+                setErrorAlert={setErrorAlert}
             />
-            <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+            <NumberOfEvents
+                setCurrentNOE={setCurrentNOE}
+                setErrorAlert={setErrorAlert}
+            />
             <EventList events={events} />
         </div>
     );
