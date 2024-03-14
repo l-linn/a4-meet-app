e0d6d0f8c29e51db86ddd58d32982537853f185c
lin <lin7cb@outlook.com>
2024-03-14 20:50:05 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { CityEventsChart } from './components/CityEventsChart';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 
@@ -44,7 +45,7 @@ const App = () => {
     return (
         <div className='App'>
             <div className='Header'>
-                <h1>Meet</h1>
+                <h1>Meet APP</h1>
                 <p className='subtitle'>Choose your nearest city</p>
             </div>
             <div className='alerts-container'>
@@ -64,6 +65,7 @@ const App = () => {
                 setCurrentNOE={setCurrentNOE}
                 setErrorAlert={setErrorAlert}
             />
+            <CityEventsChart allLocations={allLocations} events={events} />
             <EventList events={events} />
         </div>
     );
