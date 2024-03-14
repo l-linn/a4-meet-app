1719baf3b770c16801c54992b033e4bd316a3e15
lin <lin7cb@outlook.com>
2024-03-14 18:58:38 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -11,6 +11,11 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    if (!navigator.onLine) {
+        const events = localStorage.getItem('lastEvents');
+        return events ? JSON.parse(events) : [];
+    }
+
     const token = await getAccessToken();
 
     if (token) {
@@ -22,6 +27,7 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            localStorage.setItem('lastEvents', JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
