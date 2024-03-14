666034ff3840288e55c62b21fc79017068ec6cf7
lin <lin7cb@outlook.com>
2024-03-14 20:58:45 +0000

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -17,3 +17,19 @@ console.error = (...args) => {
     );
     if (!ignoreMessage) originalError(...args);
 };
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    //@ts-ignore
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+    }));
+});
+
+afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+});
